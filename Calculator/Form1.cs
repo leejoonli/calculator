0ddca5d81ae739af709d70b7a81a008d541a17b1
leejoonli <git@github.com>
2022-08-12 16:14:13 -0700

    add method for putting inputing numbers

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -18,70 +18,68 @@ namespace Calculator
 
         }
 
-        private void one_Click(object sender, EventArgs e)
+        private void testing(object sender)
         {
+            Button temp = sender as Button;
             if (operation == '+' || operation == '-' || operation == '*' || operation == '/')
             {
                 this.textBox1.Text = "";
-                operand_two += "1";
-                input += "1";
+                operand_two += temp.Text;
+                input += temp.Text;
                 this.textBox1.Text = input;
             }
             else
             {
                 this.textBox1.Text = "";
-                operand_one += "1";
-                input += "1";
-                this.textBox1.Text = operand_one;
+                operand_one += temp.Text;
+                input += temp.Text;
+                this.textBox1.Text = input;
             }
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            testing(sender);
         }
 
         private void two_Click(object sender, EventArgs e)
         {
-            input += "2";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void three_Click(object sender, EventArgs e)
         {
-            input += "3";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void four_Click(object sender, EventArgs e)
         {
-            input += "4";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void five_Click(object sender, EventArgs e)
         {
-            input += "5";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void six_Click(object sender, EventArgs e)
         {
-            input += "6";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void seven_Click(object sender, EventArgs e)
         {
-            input += "7";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void eight_Click(object sender, EventArgs e)
         {
-            input += "8";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void nine_Click(object sender, EventArgs e)
         {
-            input += "9";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -96,8 +94,7 @@ namespace Calculator
 
         private void zero_Click(object sender, EventArgs e)
         {
-            input += "0";
-            this.textBox1.Text = input;
+            testing(sender);
         }
 
         private void dot_Click(object sender, EventArgs e)
